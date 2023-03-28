267a07a4496cb2ecf002def19e77cfeab8223ae1
Peliah <pelepoupa@gmail.com>
2023-03-28 06:01:17 +0100

    Installed emailjs and made the contact functional

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { BsMailbox, BsTwitter } from 'react-icons/bs'
-
+import emailjs from 'emailjs-com'
+import AOS from 'aos'
 const Contacts = () => {
+    const [to_name, setTo_Name] = useState("")
+    const [from_name, setFrom_Name] = useState("")
+    const [from_email, setFrom_Email] = useState("")
+    const [title, setTitle] = useState("")
+    const [message, setMessage] = useState("")
+    const form = useRef()
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+
+        emailjs.sendForm('service_f3lh734', 'template_h9gauji', form.current, 'gU9gRFNbI9mBujM3m')
+        .then((result) => {
+            console.log(result.text);
+            alert("mail sent successfully");
+        }, (error) => {
+            console.log(error.text);
+            alert("Invalid details, please try again");
+        });
+    }
+    useEffect(() => {
+        AOS.init();
+      });
   return (
     <section className='flex flex-col px-6 pb-10' id='contact' style={{color:"#ABB2BF"}}>
         <section className='lg:w-1/4 w-72' >
@@ -35,17 +57,17 @@ const Contacts = () => {
                 </figure>
             </div>
         </section>
-        <form className='flex flex-col lg:w-6/12 w-full' >
+        <form ref={form} className='flex flex-col lg:w-6/12 w-full' onSubmit={handleSubmit}>
             <section className='flex lg:flex-row flex-col  justify-between my-3'>
                 <input placeholder='Name' className=' border h-10 px-2 py-2 lg:w-72 w-full' style={{background:"#282C33"}}
-                name='name' required />
+                name='from_name' required onChange={(e)=>{setFrom_Name(e.target.value)}}/>
                 <input placeholder='Email' className=' border  h-10 px-2 py-2 lg:mt-0 mt-4 lg:w-64 w-full' style={{background:"#282C33"}}
-                name='email' type='email' required />
+                name='from_email' type='email' required onChange={(e)=>{setFrom_Email(e.target.value)}}/>
             </section>
             <input placeholder='Title' className=' border h-10 px-2 py-2 lg:w-full w-full' style={{background:"#282C33"}}
-            />
+            name='title' onChange={(e)=>{setTitle(e.target.value)}}/>
             <textarea placeholder='Message'className=' border h-28 flex items-start justify-start px-2 mb-6 my-5 lg:w-full w-full' style={{background:"#282C33"}}
-            name='message' required/>
+            name='message' required onChange={(e)=>{setMessage(e.target.value)}}/>
             <button className="mb-2 animate-bounce border border-primary h-10 w-32 hover:bg-black" style={{borderColor:"#C778DD"}}>
                 contact me !!
             </button>
